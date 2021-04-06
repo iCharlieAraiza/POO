32e34097ee1c33a63a9b8b3c0fdd2f4048363d7d
iCharlieAraiza <eliptikman1234@gmail.com>
2021-04-06 02:58:26 -0500

    Create lwup.cpp

---
+++ logica/A7/lwup.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+void mayusculas(char cadena[]);
+void minusculas(char cadena[]);
+
+int main()
+{
+    int op;
+    char cadena[50];
+    
+    printf("Escribe algo (máximo 50 caracteres): \n");
+    scanf(" %[^\n]s",cadena);
+
+    printf("Elige la opción:\n1) Convertir a mayuscula  2) Convertir a minusculas\n");
+    scanf("%d", &op);
+    
+    if(op == 1){
+        mayusculas(cadena);
+    }else if(op == 2){
+        minusculas(cadena);
+    }else{
+        printf("Ups! Has elegido un valor no válido.");
+    }
+
+    return 0;
+}
+
+
+void mayusculas(char cadena[]){
+    char final[50];
+    int cont= 0;
+    
+    for(int i = 0; i<49; i++ ){
+        final[i] = cadena[i] >= 97 ? cadena[i]-32 : cadena[i];
+    }
+    printf("%s", final);
+}
+
+
+void minusculas(char cadena[]){
+    char final[50];
+    int cont= 0;
+    
+    for(int i = 0; i<49; i++ ){
+        final[i] = cadena[i] <= 90 && cadena[i]>32 ? cadena[i]+32 : cadena[i];
+    }
+    printf("%s", final);
+}
