bf348e40bbe9a3a361a950987dc26aec418c29f0
iCharlieAraiza <eliptikman1234@gmail.com>
2021-04-08 04:46:23 -0500

    Update lwup.cpp

--- logica/A7/lwup.cpp
+++ logica/A7/lwup.cpp
@@ -1,3 +1,12 @@
+/**
+    *************
+    * Aclaración*
+    * ***********
+    Los programas corren correctamente desde www.onlinegdb.com/online_c++_compiler o en compiladores estándares de C/C++. 
+    En compiladores propietarios como los de Borland o Dev C++, puede que no funcionen correctamente.
+**/
+
+
 #include <iostream>
 
 using namespace std;
